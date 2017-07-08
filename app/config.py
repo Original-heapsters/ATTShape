79f72ad42580cb705232fc7ce7545b63ffc21bec
Russell <sell_nat@yahoo.com>
2017-07-08 15:27:10 -0700

    Got video rendering working

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ ConfigVars['MockForFE'] = 1
 # 2 = Write to console only
 # 3 = Write to log and console
 ConfigVars ['LogLevel'] = 2
+ConfigVars['VideoURL'] = 'https://www.youtube.com/watch?v=akwGjUeU6YA'
